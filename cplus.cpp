df0c95fbc389d156c1b30002bd9834135051cc3e
imfitrpc <abraham.harsek@gmail.com>
2017-07-26 00:01:53 -0500

    Added first c++ test for tamarind

---
+++ cplus.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "Hello World!" << endl;
+cout << "This is my first program \n";
+  int x = 0;
+  while x < 5 {
+    cout << "variable value = " x << endl;
+    x++
+  }
+return 0;
+}
